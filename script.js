30f12788f33affffbe0ef19c43dbdb63410edd79
Lucas-New-Coder <luska2905@outlook.com>
2023-08-17 17:17:14 -0300

    Pretty much the final project the only thing left to do is add the +/- function

---
+++ script.js
@@ -0,0 +1,83 @@
+let buttons = document.querySelectorAll("button")
+let currentValue = document.querySelector('.currentValue')
+let previousValue = document.querySelector('.previousValue')
+let currentSign = ''
+
+buttons.forEach((button)=>{
+button.addEventListener("click",()=>{
+    if(!isNaN(button.innerText)||button.innerText=='.'){
+        currentValue.innerText+=button.innerText
+        }
+  
+      
+        if(previousValue.innerText==''&&isNaN(button.innerText)&&button.innerText!=='.'&&button.innerText!=='⌫'&&button.innerText!=='c'){
+            currentSign=button.innerText
+            console.log(currentSign)
+            previousValue.innerText=currentValue.innerText
+            currentValue.innerText=''
+            
+        } else if(currentSign=='+'&&isNaN(button.innerText)&&button.innerText!=='.'&&button.innerText!=='⌫'&&button.innerText!=='c'){
+            previousValue.innerText=Number(currentValue.innerText)+Number(previousValue.innerText)
+            currentValue.innerText=''
+            currentSign=button.innerText
+            if(button.innerText=='='){
+                currentValue.innerText=previousValue.innerText
+                previousValue.innerText=''
+            }
+        }
+        
+        else if(currentSign=='-'&&isNaN(button.innerText)&&button.innerText!=='.'&&button.innerText!=='⌫'&&button.innerText!=='c'){
+            previousValue.innerText=Number(previousValue.innerText)-Number(currentValue.innerText)
+            currentValue.innerText=''
+            currentSign=button.innerText
+            if(button.innerText=='='){
+                currentValue.innerText=previousValue.innerText
+                previousValue.innerText=''
+            }
+            
+        }
+        
+        else if(currentSign=='/'&&isNaN(button.innerText)&&button.innerText!=='.'&&button.innerText!=='⌫'&&button.innerText!=='c'){
+            previousValue.innerText=Number(previousValue.innerText)/Number(currentValue.innerText)
+            currentValue.innerText=''
+            currentSign=button.innerText
+         
+            if(button.innerText=='='){
+                currentValue.innerText=previousValue.innerText
+                previousValue.innerText=''
+            }
+           
+        }
+        
+        else if(currentSign=='*'&&isNaN(button.innerText)&&button.innerText!=='.'&&button.innerText!=='⌫'&&button.innerText!=='c'){
+            previousValue.innerText=Number(previousValue.innerText)*Number(currentValue.innerText)
+            currentValue.innerText=''
+            currentSign=button.innerText
+            if(button.innerText=='='){
+                currentValue.innerText=previousValue.innerText
+                previousValue.innerText=''
+            }
+        }
+        if(currentValue.innerText==='0'&&currentSign=='/'){alert('Nice try')
+        currentValue.innerText=''
+        previousValue.innerText=''
+    }   if(button.innerText=='cc'){
+        currentValue.innerText=''
+        previousValue.innerText=''
+    }if(button.innerText=='c'){
+        currentValue.innerText=''
+        
+    }if(button.innerText==='⌫'){
+        
+        currentValue.innerText = currentValue.innerText.slice(0, -1)
+        
+    }
+    
+           
+    
+           
+
+
+
+})
+})
