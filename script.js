ce5e8387c6c7b10d065396a80e051f5020f36377
luska new coder <luska2905@outlook.com>
2024-06-25 18:24:09 -0300

    Refatorei todo o codigo, mais facil de ler e entender

--- script.js
+++ script.js
@@ -1,94 +1,133 @@
-let buttons = document.querySelectorAll("button")
+const buttons = document.querySelectorAll(`button`)
+let buttonValue = ``
 let currentValue = document.querySelector('.currentValue')
 let previousValue = document.querySelector('.previousValue')
-let currentSign = ''
+let symbol = ``
+let operators = ``
 let negative = false
+let dot = false
 
-buttons.forEach((button)=>{
-button.addEventListener("click",()=>{
-    if(!isNaN(button.innerText)||button.innerText=='.'){
-        currentValue.innerText+=button.innerText
+getButtonValue = () => {
+    buttons.forEach((button) => {
+        button.addEventListener(`click`, () => {
+            buttonValue = button.innerText
+            operate()
+            getNumber()
+            getSymbol()
+            getDot()
+console.log(operators)
+
+        })
+    })
+}
+
+
+
+getNumber = () => {
+
+    if (previousValue.innerText === `` && !isNaN(buttonValue)  ) {
+        currentValue.innerText += buttonValue
+
+    }
+     if (previousValue.innerText !== `` && !isNaN(buttonValue) ) {
+        currentValue.innerText += buttonValue
+        symbol = ``
+
+    }
+
+
+
+}
+ 
+getDot = () =>{
+    
+    if (buttonValue === `.` && dot === false){
+        currentValue.innerText = currentValue.innerText+`.`
+    }
+}
+
+getSymbol = () => {
+
+
+     if (isNaN(buttonValue) && buttonValue!== `.`) {
+        symbol = buttonValue
+        operators = symbol
+        if (symbol === `⌫`) {
+            currentValue.innerText = currentValue.innerText.slice(0, -1)
+        } else if (negative === false && symbol === `+/-`) {
+            currentValue.innerText = '-' + currentValue.innerText
+            negative = true
+
+        } else if (negative === true) {
+            currentValue.innerText = currentValue.innerText.slice(1)
+            negative = false
+        } else if ( buttonValue === `c`){
+            currentValue.innerText = ``
+        } 
+         else if (previousValue.innerText === ``){
+            previousValue.innerText = currentValue.innerText
+            currentValue.innerText = ``
         }
+    }
+
+    if (buttonValue === `cc`) {
+        currentValue.innerText = ''
+        previousValue.innerText = ''
+    }
+
+   
+
   
-      
-        if(previousValue.innerText==''&&isNaN(button.innerText)&&button.innerText!=='.'&&button.innerText!=='⌫'&&button.innerText!=='c'&&button.innerText!=='+/-'){
-            currentSign=button.innerText
-            console.log(currentSign)
-            previousValue.innerText=currentValue.innerText
-            currentValue.innerText=''
-            
-        } else if(currentSign=='+'&&isNaN(button.innerText)&&button.innerText!=='.'&&button.innerText!=='⌫'&&button.innerText!=='c'&&button.innerText!=='+/-'){
-            previousValue.innerText=Number(currentValue.innerText)+Number(previousValue.innerText)
-            currentValue.innerText=''
-            currentSign=button.innerText
-            if(button.innerText=='='){
-                currentValue.innerText=previousValue.innerText
-                previousValue.innerText=''
-            }
+
+}
+
+
+operate = () => {
+
+    if (symbol === `` && isNaN(buttonValue) && operators !== `` && buttonValue !==`.`) {
+
+        switch (operators) {
+            case `+`:
+                previousValue.innerText = Number(currentValue.innerText) + Number(previousValue.innerText)
+                currentValue.innerText = ``
+                break;
+
+            case `-`:
+                previousValue.innerText = Number(previousValue.innerText) - Number(currentValue.innerText)
+                currentValue.innerText = ``
+                break;
+            case `*`:
+                previousValue.innerText = Number(currentValue.innerText) * Number(previousValue.innerText)
+                currentValue.innerText = ``
+                break;
+
+            case `/`:
+
+                if (currentValue.innerText === `0`) {
+                    currentValue.innerText = ``;
+                    previousValue.innerText = ``;
+                    alert(`Can´t divide by 0`)
+
+                }
+                previousValue.innerText = Number(previousValue.innerText) / Number(currentValue.innerText)
+                currentValue.innerText = ``
+                break;
+
         }
-        
-        else if(currentSign=='-'&&isNaN(button.innerText)&&button.innerText!=='.'&&button.innerText!=='⌫'&&button.innerText!=='c'&&button.innerText!=='+/-'){
-            previousValue.innerText=Number(previousValue.innerText)-Number(currentValue.innerText)
-            currentValue.innerText=''
-            currentSign=button.innerText
-            if(button.innerText=='='){
-                currentValue.innerText=previousValue.innerText
-                previousValue.innerText=''
-            }
-            
-        }
-        
-        else if(currentSign=='/'&&isNaN(button.innerText)&&button.innerText!=='.'&&button.innerText!=='⌫'&&button.innerText!=='c'&&button.innerText!=='+/-'){
-            previousValue.innerText=Number(previousValue.innerText)/Number(currentValue.innerText)
-            currentValue.innerText=''
-            currentSign=button.innerText
-         
-            if(button.innerText=='='){
-                currentValue.innerText=previousValue.innerText
-                previousValue.innerText=''
-            }
-           
-        }
-        
-        else if(currentSign=='*'&&isNaN(button.innerText)&&button.innerText!=='.'&&button.innerText!=='⌫'&&button.innerText!=='c'&&button.innerText!=='+/-'){
-            previousValue.innerText=Number(previousValue.innerText)*Number(currentValue.innerText)
-            currentValue.innerText=''
-            currentSign=button.innerText
-            if(button.innerText=='='){
-                currentValue.innerText=previousValue.innerText
-                previousValue.innerText=''
-            }
-        }
-        if(currentValue.innerText==='0'&&currentSign=='/'){alert('Nice try')
-        currentValue.innerText=''
-        previousValue.innerText=''
-    }   if(button.innerText=='cc'){
-        currentValue.innerText=''
-        previousValue.innerText=''
-    }if(button.innerText=='c'){
-        currentValue.innerText=''
-        
-    }if(button.innerText==='⌫'){
-        
-        currentValue.innerText = currentValue.innerText.slice(0, -1)
-        
     }
-    if(button.innerText==='+/-'&&negative==false){
-        
-        currentValue.innerText = '-'+currentValue.innerText
-        negative = true
-        
-    }else if (button.innerText==='+/-'&&negative==true){
-        currentValue.innerText = currentValue.innerText.slice(1)
-        negative=false
-        console.log(negative)
+
+    if (symbol === `` && buttonValue === `=`) {
+        currentValue.innerText = `${Number(previousValue.innerText)} `
+        previousValue.innerText = `${Number(previousValue.innerText)}`
     }
-    
-           
-    
-           
+
+
+}
 
 
 
-})
-})
+
+
+
+
+getButtonValue()
